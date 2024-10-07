e9b0b35f426288b738dfc33d32bcebdc5833582f
kaamitan <azibekovakamila18@gmail.com>
2024-10-07 18:38:02 +0600

    Add files via upload

--- backand/homework/mouth-2/work-1/main.py
+++ backand/homework/mouth-2/work-1/main.py
@@ -1,4 +1,4 @@
-class SuperHero():
+class SuperHero:
     people = 'people'
 
     def __init__(self, name, nickname, superpower, health_points: int, catchphrase):
@@ -7,26 +7,71 @@ class SuperHero():
         self.superpower = superpower
         self.health_points = health_points
         self.catchphrase = catchphrase
+        self.fly = False
 
     def get_name(self):
         return self.name
 
     def duble_health(self):
-        self.health_points *= 2
+        self.health_points **= 2
+        self.fly = True
         return self.health_points
 
     def __str__(self):
-        return f'\nname: {self.name}\nnick_name: {self.nickname}\nsuper_power:{self.superpower}\nhealth: {self.health_points}\ncatch_phrase: {self.catchphrase}'
+        return f'\nname: {self.name}\nnick_name: {self.nickname}\nsuper_power: {self.superpower}\nhealth: {self.health_points}\ncatch_phrase: {self.catchphrase}'
 
     def __len__(self):
         return len(self.catchphrase)
 
+    def true_phrase(self):
+        if self.fly:
+            return f'True in the True_phrase'
 
-hero = SuperHero("Питер Паркер", "Человек Паук", "Путина",
-                 100, "С вами ваш Дружулюбный Сосед Ч.П ")
+# Воздушный герой
+class AirHero(SuperHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
 
-getName = hero.get_name()
-healthPoint = hero.duble_health()
-lenCatphrase = len(hero)
+    # Метод для возведения здоровья в квадрат
+    def duble_health(self):
+        self.health_points **= 2
+        self.fly = True
+        return self.health_points
 
-print(hero)
+# Земной герой
+class EarthHero(SuperHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+
+    def duble_health(self):
+        self.health_points **= 2
+        self.fly = True
+        return self.health_points
+
+# Класс злодея
+class Villain(SuperHero):
+    people = 'monster'
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+
+    def gen_x(self):
+        pass
+
+    def crit(self, target):
+        return target.damage ** 2
+
+# Создаем объекты героев и злодея
+air_hero = AirHero("Джон", "Летун", "Ветер", 150, "Я выше всех", 50)
+earth_hero = EarthHero("Марк", "Земляной", "Сила земли", 200, "Я стою на земле крепко", 70)
+villain = Villain("Локи", "Мастер иллюзий", "Иллюзии", 120, "Я сделаю хаос", 80)
+
+# Применяем методы
+print(air_hero.duble_health())  # Возведение здоровья в квадрат
+print(air_hero.true_phrase())   # Проверка полиморфизма
+print(earth_hero.duble_health())  # Возведение здоровья в квадрат
+print(earth_hero.true_phrase())   # Проверка полиморфизма
+print(villain.crit(earth_hero))  # Применение метода crit к герою
